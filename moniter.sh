8281bb31b8899ea20c7fb57692714b65ebc6f244
root <root@ip-10-2-1-180.alldomain>
2020-05-28 09:52:44 +0000

    ok

--- moniter.sh
+++ moniter.sh
@@ -1,6 +1,6 @@
-echo "Date, RAM(MB), CPU(%)" >> cpuRamUsage3.csv
+#echo "Date, RAM(MB), CPU(%)" >> cpuRamUsage3.csv
 while :
 do
 echo "$(date),  $(free -m | awk 'NR==2{printf $3}'), $(top bn1 | awk 'NR==3{printf  $2}')" >> cpuRamUsage3.csv
-sleep 10
+sleep 60
 done
